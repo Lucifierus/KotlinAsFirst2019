81e3d42eabae8c06e8fb2817c2b60cf7c6036882
Sergei Anciferov <sergozas@yandex.ru>
2019-12-05 22:58:41 +0300

    Lesson 8. H E L P

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -164,7 +164,13 @@ class Line private constructor(val b: Double, val angle: Double) {
      * Найти точку пересечения с другой линией.
      * Для этого необходимо составить и решить систему из двух уравнений (каждое для своей прямой)
      */
-    fun crossPoint(other: Line): Point = TODO()
+    fun crossPoint(other: Line): Point {
+        val divisor1 = sin(angle) * cos(other.angle) - sin(other.angle) * cos(angle)
+        val divisor2 = cos(angle) * sin(other.angle) - cos(other.angle) * sin(angle)
+        val x = (other.b * cos(angle) - b * cos(other.angle)) / divisor1
+        val y = (sin(other.angle) * b - sin(angle) * other.b) / divisor2
+        return Point(x, y)
+    }
 
     override fun equals(other: Any?) = other is Line && angle == other.angle && b == other.b
 
