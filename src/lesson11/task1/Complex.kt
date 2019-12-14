552bf0c51d9a1e0ae3758741b44d87920ed8337a
Sergei Anciferov <sergozas@yandex.ru>
2019-12-14 21:18:56 +0300

    Lesson 10.

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -2,6 +2,8 @@
 
 package lesson11.task1
 
+import lesson1.task1.sqr
+
 /**
  * Класс "комплексое число".
  *
@@ -16,42 +18,49 @@ class Complex(val re: Double, val im: Double) {
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x, 0.0)
 
     /**
      * Конструктор из строки вида x+yi
      */
-    constructor(s: String) : this(TODO(), TODO())
+    constructor(s: String) : this(
+        Regex("""([-+]?)(\d+)(\.?\d*)""").findAll(s).elementAt(0).value.toDouble(),
+        Regex("""([-+]?)(\d+)(\.?\d*)""").findAll(s).elementAt(1).value.toDouble()
+    )
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex): Complex = Complex(re + other.re, im + other.im)
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus(): Complex = Complex(-im, -re)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex = Complex(re - other.re, im - other.im)
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex =
+        Complex(re * other.re.minus(im * other.im), im * other.re.plus(re * other.im))
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+    operator fun div(other: Complex): Complex = Complex(
+        (re * other.re.plus(im * other.im)) / (sqr(other.re).plus(sqr(other.im))),
+        (im * other.re.minus(re * other.im)) / (sqr(other.re).plus(sqr(other.im)))
+    )
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is Complex && re == other.re && im == other.im
 
     /**
      * Преобразование в строку
